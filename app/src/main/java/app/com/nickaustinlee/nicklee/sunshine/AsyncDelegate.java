3d7232118853cd63f067f587f28edd7b4e34f998
Nick Lee <fabbuki@gmail.com>
2015-06-06 21:06:37 -0700

    Wow I did it
    Wow! I used an asynchronous delegate to pull the async data fetch out
    of the Forecast Fragment. It’s a bit jenky, but the code works.

---
+++ app/src/main/java/app/com/nickaustinlee/nicklee/sunshine/AsyncDelegate.java
@@ -0,0 +1,8 @@
+package app.com.nickaustinlee.nicklee.sunshine;
+
+/**
+ * Created by nicklee on 6/6/15.
+ */
+public interface AsyncDelegate {
+    public void asyncComplete(boolean success);
+}
---
+++ app/src/main/java/app/com/nickaustinlee/nicklee/sunshine/ConvertWeatherJSON.java
@@ -0,0 +1,7 @@
+package app.com.nickaustinlee.nicklee.sunshine;
+
+/**
+ * Created by nicklee on 6/6/15.
+ */
+public class ConvertWeatherJSON {
+}
